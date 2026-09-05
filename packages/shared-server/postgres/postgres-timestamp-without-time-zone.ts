dbb884eb6e82d54f446131799760471433f2bc56
Knut-Helge Vik <intact.software.systems@gmail.com>
2026-09-05 19:12:57 +0200

    fix(postgres): name timestamp type contract (#501)

--- packages/shared-server/postgres/postgres-timestamp-without-time-zone.ts
+++ packages/shared-server/postgres/postgres-timestamp-without-time-zone.ts
@@ -1,6 +1,13 @@
 export const POSTGRES_TIMESTAMP_WITHOUT_TIME_ZONE_OID = 1114;
 
-export function createPostgresTimestampWithoutTimeZoneTextType() {
+export interface PostgresTimestampWithoutTimeZoneTextType {
+    readonly to: number;
+    readonly from: number[];
+    readonly serialize: (value: string) => string;
+    readonly parse: (value: string) => string;
+}
+
+export function createPostgresTimestampWithoutTimeZoneTextType(): PostgresTimestampWithoutTimeZoneTextType {
     return {
         to: POSTGRES_TIMESTAMP_WITHOUT_TIME_ZONE_OID,
         from: [POSTGRES_TIMESTAMP_WITHOUT_TIME_ZONE_OID],
